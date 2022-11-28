40fe6399b25a4ff2b1e71f630def8dfa459f7753
Xander Low <xander@xandydandy.com>
2022-11-28 00:13:19 -0800

    added: new secrets

--- environment.d.ts
+++ environment.d.ts
@@ -1,5 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_RSS_URL: string
+    NEXT_PUBLIC_SANITY_PROJECT_ID: string
+    NEXT_PUBLIC_SANITY_DATASET: string
+    NEXT_PUBLIC_SANITY_API_VERSION: string
   }
 }
