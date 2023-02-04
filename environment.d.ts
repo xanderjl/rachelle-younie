e3f2d465c32e42b53e4d463eb83f2738558b1872
Xander Low <xander@xandydandy.com>
2023-02-03 16:01:02 -0800

    updated: base url

--- environment.d.ts
+++ environment.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
     VERCEL_ENV: 'production' | 'preview' | 'development'
     VERCEL_URL: string
     NEXT_PUBLIC_RSS_URL: string
+    NEXT_PUBLIC_VERCEL_URL: string
     NEXT_PUBLIC_SANITY_PROJECT_ID: string
     NEXT_PUBLIC_SANITY_DATASET: string
     NEXT_PUBLIC_SANITY_API_VERSION: string
--- studio/defaultDocumentNode.ts
+++ studio/defaultDocumentNode.ts
@@ -1,9 +1,10 @@
 import type { DefaultDocumentNodeResolver } from 'sanity/desk'
 import Iframe from 'sanity-plugin-iframe-pane'
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (
   S,
