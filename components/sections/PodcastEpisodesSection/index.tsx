907125a7f461a4e8cbe05ab17d21fe13ebd15210
Xander Low <xander@xandydandy.com>
2023-02-07 08:32:32 -0800

    chore: complete pagination rewrite' gp

--- components/sections/PodcastEpisodesSection/index.tsx
+++ components/sections/PodcastEpisodesSection/index.tsx
@@ -1,10 +1,9 @@
 import type { FlexProps } from '@chakra-ui/react'
 import { Button, Flex } from '@chakra-ui/react'
 import type { SectionPodcastEpisodes } from 'hooks/data/useGetPage'
+import { usePagination } from 'hooks/usePagination'
 import type { FC } from 'react'
-import { useState } from 'react'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
-import { paginateArray } from 'utils/paginateArray'
 
 import PodcastCard from './PodcastCard'
 
@@ -16,34 +15,29 @@ export const PodcastEpisodesSection: FC<PodcastEpisodesSectionProps> = ({
   episodes,
   ...rest
 }) => {
-  const [currentPage, setCurrentPage] = useState<number>(0)
-  const pageSize = 10
+  const {
+    currentData: slicedEpisodes,
+    next,
+    prev,
+    jump
+  } = usePagination(episodes, 10)
 
-  if (episodes) {
-    const { array: slicedEpisodes, numPages } = paginateArray(
-      episodes,
-      currentPage,
-      pageSize
-    )
-
-    return (
-      <Flex direction='column' gap={6} {...rest}>
-        {slicedEpisodes?.map(episode => (
-          <PodcastCard key={episode._key} episode={episode} />
-        ))}
-        <Flex flex={1} maxW='70ch' justify='space-between' gap={6}>
-          <Button
-            as={IoIosArrowBack}
-            onClick={() => setCurrentPage(currentPage - 1)}
-          />
-          <Button
-            as={IoIosArrowForward}
-            onClick={() => setCurrentPage(currentPage + 1)}
-          />
-        </Flex>
+  return (
+    <Flex direction='column' gap={6} {...rest}>
+      {slicedEpisodes?.map(episode => (
+        <PodcastCard key={episode._key} episode={episode} />
+      ))}
+      <Flex flex={1} maxW='70ch' justify='space-between' gap={6}>
+        <Button as={IoIosArrowBack} colorScheme='burntOrange' onClick={next} />
+        <Button colorScheme='burntOrange' onClick={() => jump(2)}>
+          FUCK
+        </Button>
+        <Button
+          as={IoIosArrowForward}
+          colorScheme='burntOrange'
+          onClick={prev}
+        />
       </Flex>
-    )
-  }
-
-  return <></>
+    </Flex>
+  )
 }
---
+++ hooks/usePagination.ts
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+/**
+ * @param arr
+ * Array of data you wish to paginate.
+ *
+ * @param pageSize
+ * How many items should be returned for a given page.
+ */
+export type Pagination = <T>(
+  arr: T[] | undefined,
+  pageSize: number
+) => PaginationReturnType<T>
+
+/**
+ * @param array
+ * The returned array, sliced based on the index of   `currentPage`.
+ *
+ * @param currentPage
+ * The index of the current page in `arr`.
+ *
+ * @param numPages
+ * The maximum number of pages based on the length of `arr` divisible by `pageSize`.
+ */
+export interface PaginationReturnType<T> {
+  next: () => void
+  prev: () => void
+  jump: (page: number) => void
+  currentData: T[]
+  currentPage: number
+  numPages: number
+}
+
+export const usePagination: Pagination = (arr = [], pageSize) => {
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const numPages = Math.ceil(arr.length / pageSize)
+
+  const start = (currentPage - 1) * pageSize
+  const end = start + pageSize
+  const currentData = arr.slice(start, end)
+
+  const next = () =>
+    setCurrentPage(currentPage => Math.min(currentPage + 1, numPages))
+
+  const prev = () => setCurrentPage(currentPage => Math.max(currentPage - 1, 1))
+
+  const jump = (page: number) => {
+    const pageNumber = Math.max(1, page)
+
+    setCurrentPage(currentPage => Math.min(pageNumber, numPages))
+  }
+
+  return {
+    next,
+    prev,
+    jump,
+    currentData,
+    currentPage,
+    numPages
+  }
+}
