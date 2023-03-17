8171c971222ea99f3c8c30afb9f594bf3c59d0e3
Xander Low <xander@xandydandy.com>
2023-03-16 22:18:25 -0700

    added: formspree deps

--- environment.d.ts
+++ environment.d.ts
@@ -3,11 +3,12 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test'
     VERCEL_ENV: 'production' | 'preview' | 'development'
     VERCEL_URL: string
+    NEXT_PUBLIC_FORMSPREE_KEY: string
+    NEXT_PUBLIC_SANITY_API_VERSION: string
+    NEXT_PUBLIC_SANITY_DATASET: string
+    NEXT_PUBLIC_SANITY_PROJECT_ID: string
     NEXT_SANITY_API_TOKEN: string
     NEXT_PUBLIC_RSS_URL: string
     NEXT_PUBLIC_VERCEL_URL: string
-    NEXT_PUBLIC_SANITY_PROJECT_ID: string
-    NEXT_PUBLIC_SANITY_DATASET: string
-    NEXT_PUBLIC_SANITY_API_VERSION: string
   }
 }
