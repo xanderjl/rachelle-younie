81b82aadb8c2beeb356e72b5b971e2d4281c1b7d
Xander Low <xander@xandydandy.com>
2023-03-20 14:37:48 -0700

    refactor: removed landingpage hooks and queries. redundant

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,16 @@
 import type { DehydratedState } from '@tanstack/react-query'
 import { dehydrate, QueryClient } from '@tanstack/react-query'
 import { SectionRenderer } from 'components/SectionRenderer'
-import {
-  getLandingPageData,
-  useGetLandingPage
-} from 'hooks/data/useGetLandingPage'
+import { getPage, useGetPage } from 'hooks/data/useGetPage'
 import { useInitialData } from 'hooks/data/useInitialData'
 import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 import Head from 'next/head'
 import { PreviewSuspense } from 'next-sanity/preview'
 import { lazy } from 'react'
 
-const PreviewLandingPage = lazy(() =>
-  import('components/previews/PreviewLandingPage').then(mod => ({
-    default: mod.PreviewLandingPage
+const PreviewPage = lazy(() =>
+  import('components/previews/PreviewPage').then(mod => ({
+    default: mod.PreviewPage
   }))
 )
 
@@ -25,14 +22,14 @@ export interface StaticProps {
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   preview
 }) => {
-  const { data } = useGetLandingPage()
+  const { data } = useGetPage('home')
   const { data: initialData } = useInitialData()
   const { title, metaDescription, sections } = data || {}
   const { siteTitle } = initialData || {}
 
   return preview ? (
     <PreviewSuspense fallback='Loading...'>
-      <PreviewLandingPage />
+      <PreviewPage />
     </PreviewSuspense>
   ) : (
     <>
@@ -53,11 +50,17 @@ export const getStaticProps: GetStaticProps<StaticProps> = async ({
   preview = false
 }) => {
   const queryClient = new QueryClient()
-  await queryClient.prefetchQuery(['landing-page'], getLandingPageData)
 
   if (preview) {
     return { props: { preview } }
   }
+
+  await queryClient.prefetchQuery({
+    queryKey: ['page', 'home'],
+    queryFn: () => getPage('home'),
+    staleTime: Infinity,
+    cacheTime: Infinity
+  })
 
   return {
     props: {
