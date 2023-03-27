fb793966e1f67cc7ddaf453096a6b42d3a4dea50
Xander Low <xander@xandydandy.com>
2023-03-26 20:18:11 -0700

    updated: contact seo

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,10 +11,23 @@ import {
 } from '@chakra-ui/react'
 import { useForm, ValidationError } from '@formspree/react'
 import { Section } from 'components/Section'
-import type { GetStaticProps, NextPage } from 'next'
-import type { FC } from 'react'
+import type { InitialData } from 'hooks/data/useInitialData'
+import { getInitialData } from 'hooks/data/useInitialData'
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import type { NextSeoProps } from 'next-seo'
+import { NextSeo } from 'next-seo'
+import baseUrl from 'utils/baseUrl'
+import { createOgImageUrl } from 'utils/createOgImageUrl'
 
-const ContactPage: FC<NextPage> = () => {
+export interface StaticProps {
+  fallback: {
+    '/sanity/initialData': InitialData
+  }
+}
+
+const ContactPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  fallback
+}) => {
   const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORMSPREE_KEY)
 
   const sharedStyles: BoxProps = {
@@ -25,77 +38,113 @@ const ContactPage: FC<NextPage> = () => {
     }
   }
 
+  const { siteTitle } = fallback['/sanity/initialData']
+  const title = 'contact'
+  const description = 'beeeeep'
+  const url = createOgImageUrl(siteTitle ?? '', title).toString()
+
+  const seo: NextSeoProps = {
+    title,
+    description,
+    openGraph: {
+      type: 'website',
+      url: `${baseUrl}/contact`,
+      title,
+      description,
+      images: [
+        {
+          url,
+          width: 1200,
+          height: 627
+        }
+      ]
+    },
+    twitter: {
+      handle: '@rachelleyounie',
+      site: baseUrl,
+      cardType: 'summary_large_image'
+    }
+  }
+
   return (
-    <Section>
-      {state.succeeded ? (
-        <Heading textAlign='center'>
-          Thank you for your message! I&apos;ll get back to you as soon as I
-          can.
-        </Heading>
-      ) : (
-        <>
-          <Heading size='3xl' pb={{ base: 6, md: 10 }}>
-            Get In Touch
+    <>
+      <NextSeo {...seo} />
+      <Section>
+        {state.succeeded ? (
+          <Heading textAlign='center'>
+            Thank you for your message! I&apos;ll get back to you as soon as I
+            can.
           </Heading>
-          <Flex
-            as='form'
-            display='flex'
-            flexDirection='column'
-            gap={6}
-            onSubmit={handleSubmit}
-          >
-            <FormControl isRequired>
-              <FormLabel htmlFor='email'>Email Address</FormLabel>
-              <Input type='email' name='email' {...sharedStyles} />
-              <ValidationError
-                prefix='Email'
-                field='email'
-                errors={state.errors}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel htmlFor='name'>Full Name</FormLabel>
-              <Input name='name' {...sharedStyles} />
-              <ValidationError
-                prefix='Name'
-                field='name'
-                errors={state.errors}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel htmlFor='message'>Message</FormLabel>
-              <Textarea
-                name='message'
-                rows={10}
-                {...(sharedStyles as TextareaProps)}
-              />
-              <ValidationError
-                prefix='Message'
-                field='message'
-                errors={state.errors}
-              />
-            </FormControl>
-            <FormControl>
-              <Button
-                type='submit'
-                loadingText='Submitting...'
-                isLoading={state.submitting}
-                leftIcon={state.submitting ? <Spinner /> : undefined}
-                colorScheme='burntOrange'
-              >
-                Submit
-              </Button>
-            </FormControl>
-          </Flex>
-        </>
-      )}
-    </Section>
+        ) : (
+          <>
+            <Heading size='3xl' pb={{ base: 6, md: 10 }}>
+              Get In Touch
+            </Heading>
+            <Flex
+              as='form'
+              display='flex'
+              flexDirection='column'
+              gap={6}
+              onSubmit={handleSubmit}
+            >
+              <FormControl isRequired>
+                <FormLabel htmlFor='email'>Email Address</FormLabel>
+                <Input type='email' name='email' {...sharedStyles} />
+                <ValidationError
+                  prefix='Email'
+                  field='email'
+                  errors={state.errors}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor='name'>Full Name</FormLabel>
+                <Input name='name' {...sharedStyles} />
+                <ValidationError
+                  prefix='Name'
+                  field='name'
+                  errors={state.errors}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor='message'>Message</FormLabel>
+                <Textarea
+                  name='message'
+                  rows={10}
+                  {...(sharedStyles as TextareaProps)}
+                />
+                <ValidationError
+                  prefix='Message'
+                  field='message'
+                  errors={state.errors}
+                />
+              </FormControl>
+              <FormControl>
+                <Button
+                  type='submit'
+                  loadingText='Submitting...'
+                  isLoading={state.submitting}
+                  leftIcon={state.submitting ? <Spinner /> : undefined}
+                  colorScheme='burntOrange'
+                >
+                  Submit
+                </Button>
+              </FormControl>
+            </Flex>
+          </>
+        )}
+      </Section>
+    </>
   )
 }
 export default ContactPage
 
-export const getStaticProps: GetStaticProps = () => ({
-  props: {
-    fallback: false
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
+  const initialData = await getInitialData()
+  const fallback = { '/sanity/initialData': initialData }
+
+  return {
+    props: {
+      fallback
+    }
   }
-})
+}
