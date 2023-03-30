e2646e2e92e0a8ed85b6c606bbb1f2ae7bd6fa67
Xander Low <xander@xandydandy.com>
2023-03-29 21:18:55 -0700

    added: new footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,42 +1,60 @@
 import type { ContainerProps } from '@chakra-ui/react'
-import { Container, Text } from '@chakra-ui/react'
-// import { useInitialData } from 'hooks/data/useInitialData'
+import { Container, Flex, Image, Link, Text } from '@chakra-ui/react'
+import { useInitialData } from 'hooks/data/useInitialData'
+import NLink from 'next/link'
 import type { FC } from 'react'
+import { urlFor } from 'utils/urlFor'
+
+import { linkStyles } from './Navbar/linkStyles'
 
 export const Footer: FC<ContainerProps> = props => {
-  // const { data } = useInitialData()
-  // const { navigation, socials } = data || {}
+  const { data } = useInitialData()
+  const { navigation, socials } = data || {}
   const year = new Date().getFullYear()
+  const gap = 2
 
   return (
     <Container
       as='footer'
       display='flex'
       flexDirection='column'
-      justifyContent='center'
       alignItems='center'
       maxW='container.lg'
-      py={3}
+      py={{ base: 4, md: 8 }}
+      gap={gap}
       {...props}
     >
-      {/* <Flex direction='column'>
+      <Flex gap={gap} flexWrap='wrap' pb={2}>
         {navigation?.map(({ slug, title }) => (
-          <Link key={slug} href={`/${slug}`}>
+          <Link as={NLink} key={slug} href={`/${slug}`} {...linkStyles}>
             {title}
           </Link>
         ))}
       </Flex>
-      <Flex>
-        {socials?.map(({ _id, icon, title, url }) => {
-          const src = urlFor(icon).url()
-          return (
-            <a key={_id} href={url}>
-              <Image src={src} alt={title} boxSize={8} />
-            </a>
-          )
-        })}
-      </Flex> */}
-      <Text as='span'>&copy; Copyright {year}, Rachelle Younie</Text>
+      <Flex flexDirection='column' align='center' gap={gap}>
+        <Text as='span' fontSize='sm'>
+          &copy; Copyright {year}, Rachelle Younie
+        </Text>
+        <Flex gap={gap}>
+          {socials?.map(({ _id, icon, title, url }) => {
+            const src = urlFor(icon).url()
+
+            return (
+              <Link
+                key={_id}
+                href={url}
+                filter='brightness(0) saturate(100%) invert(26%) sepia(47%) saturate(819%) hue-rotate(332deg) brightness(95%) contrast(87%)'
+                _hover={{
+                  filter:
+                    'brightness(0) saturate(100%) invert(30%) sepia(30%) saturate(1126%) hue-rotate(10deg) brightness(99%) contrast(89%)'
+                }}
+              >
+                <Image src={src} alt={title} boxSize={6} />
+              </Link>
+            )
+          })}
+        </Flex>
+      </Flex>
     </Container>
   )
 }
