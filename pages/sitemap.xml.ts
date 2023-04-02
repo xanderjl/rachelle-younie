35886904d379a9072f57852021d30cfa96c53be4
Xander Low <xander@xandydandy.com>
2023-04-02 12:49:59 -0700

    SEO change

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -38,7 +38,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(deduped)
 
-  res.setHeader('Content-Type', 'text/xml')
+  res.setHeader('Content-Type', 'application/xml')
   // we send the XML to the browser
   res.write(sitemap)
   res.end()
