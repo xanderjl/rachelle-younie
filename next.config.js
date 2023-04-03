569f7b9eac2207faad1b5a611e942aaa1293ce72
Xander Low <xander@xandydandy.com>
2023-04-03 12:26:40 -0700

    added: lang prop

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 let nextConfig = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
